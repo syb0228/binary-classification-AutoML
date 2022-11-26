2f1791bde0694bd9e36e228c842b2acadde145a6
Yebin <62875437+syb0228@users.noreply.github.com>
2022-11-26 19:01:30 +0900

    [FORM]  delete comments

--- template.py
+++ template.py
@@ -1,12 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[91]:
-
-
-#PLEASE WRITE THE GITHUB URL BELOW!
-#
-
 import sys
 import pandas as pd
 from sklearn.model_selection import train_test_split
@@ -18,24 +9,20 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import accuracy_score, precision_score, recall_score
 
 def load_dataset(dataset_path):
-	#To-Do: Implement this function
 	return pd.read_csv(dataset_path)
 
 def dataset_stat(dataset_df):	
-	#To-Do: Implement this function
 	n_feats = data_df.shape[1] - 1
 	n_class0 = data_df.groupby("target").size()[0]
 	n_class1 = data_df.groupby("target").size()[1]
 	return n_feats, n_class0, n_class1
 
 def split_dataset(dataset_df, testset_size):
-	#To-Do: Implement this function
 	x = dataset_df.drop(columns="target", axis=1)
 	y = dataset_df["target"]
 	return train_test_split(x, y, test_size=testset_size, random_state=2)
 
 def decision_tree_train_test(x_train, x_test, y_train, y_test):
-	#To-Do: Implement this function
 	dt_cls = DecisionTreeClassifier()
 	dt_cls.fit(x_train, y_train)
 	accuracy = accuracy_score(y_test, dt_cls.predict(x_test))
@@ -44,7 +31,6 @@ def decision_tree_train_test(x_train, x_test, y_train, y_test):
 	return accuracy, precision, recall
 
 def random_forest_train_test(x_train, x_test, y_train, y_test):
-	#To-Do: Implement this function
 	rf_cls = RandomForestClassifier()
 	rf_cls.fit(x_train, y_train)
 	accuracy = accuracy_score(rf_cls.predict(x_test), y_test)
@@ -53,8 +39,6 @@ def random_forest_train_test(x_train, x_test, y_train, y_test):
 	return accuracy, precision, recall
 
 def svm_train_test(x_train, x_test, y_train, y_test):
-	#To-Do: Implement this function
-	#To-Do: Implement this function
 	svm_pipe = make_pipeline(
 	    StandardScaler(),
 	    SVC()
@@ -66,13 +50,11 @@ def svm_train_test(x_train, x_test, y_train, y_test):
 	return accuracy, precision, recall
 
 def print_performances(acc, prec, recall):
-	#Do not modify this function!
 	print ("Accuracy: ", acc)
 	print ("Precision: ", prec)
 	print ("Recall: ", recall)
 
 if __name__ == '__main__':
-	#Do not modify the main script!
 	data_path = sys.argv[1]
 	data_df = load_dataset(data_path)
 
@@ -95,4 +77,3 @@ if __name__ == '__main__':
 	acc, prec, recall = svm_train_test(x_train, x_test, y_train, y_test)
 	print ("\nSVM Performances")
 	print_performances(acc, prec, recall)
-
